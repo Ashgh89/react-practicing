531c4c292c0ff4ea738fb8b3e7628159a90fea14
Iman Ghanei <ghaneiash@gmail.com>
2022-01-31 12:13:44 +0100

    changes

--- src/components/Context/CounterProvider.js
+++ src/components/Context/CounterProvider.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 export const CounterContext = React.createContext();
-const CounterProvider = () => {
-  return <CounterContext.Provider></CounterContext.Provider>;
+const CounterProvider = ({ children }) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CounterContext.Provider value={count}>{children}</CounterContext.Provider>
+  );
 };
 
 export default CounterProvider;
