bbbdc7e7e1e766c2cd257b03a5d7a10086d48934
Iman Ghanei <ghaneiash@gmail.com>
2022-02-01 18:01:00 +0100

    changes

--- src/components/Reducer/CountReducer.js
+++ src/components/Reducer/CountReducer.js
@@ -1,31 +1,55 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
-// NOTICE As you see our codes works well but we are
-// writting 3 handler for one STATE and if i have more and more handler
-// that is not going to be clean code and there will be many codes there.
-// SO what to do? useReducer hook!
-// If you want to see how useReducer works, go to the next page.
+// 5. Now define reducer function and initialState
+const initialState = 0;
+// 6. reducer function hast 2 parameters => reducer(state,action);
+// NOTICE we can write this reducer down there into useReducer(REDUCER (except this reducer we can give this function), initialState)
+const reducer = (state, action) => {
+  switch (action) {
+    case "addOne":
+      return state + 1;
+    case "addSeven":
+      return state + 7;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+// useReducer :
+// 1. Very similar to useState, so for state managment
+// 2. Reduce
+
+// Steps for useReducer :
+// 1. import useReducer() hook
+// 2. useReducer(reducer,initialState). first parameter (reducer) is a function
+// 3. return : [count,dispatch]
 const CountReducer = () => {
-  const [count, setCount] = useState(0);
+  // with useReducer we dont need this useState anymore
+  //const [count, setCount] = useState(0);
 
-  const addOne = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+  // 4. Let's do it
+  const [count, dispatch] = useReducer(reducer, initialState);
 
-  const addSix = () => {
-    setCount((prevCount) => prevCount + 6);
-  };
+  // With useReducer we don't need this 3 handlers, because we have them in switch
+  //   const addOne = () => {
+  //     setCount((prevCount) => prevCount + 1);
+  //   };
 
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+  //   const addSix = () => {
+  //     setCount((prevCount) => prevCount + 6);
+  //   };
+
+  //   const decrement = () => {
+  //     setCount((prevCount) => prevCount - 1);
+  //   };
 
   return (
     <div>
       <h2>count is {count}</h2>
-      <button onClick={addOne}>add one</button>
-      <button onClick={addSix}>add six</button>
-      <button onClick={decrement}>decrement</button>
+      <button onClick={() => dispatch("addOne")}>add one</button>
+      <button onClick={() => dispatch("addSeven")}>add seven</button>
+      <button onClick={() => dispatch("decrement")}>decrement</button>
     </div>
   );
 };
