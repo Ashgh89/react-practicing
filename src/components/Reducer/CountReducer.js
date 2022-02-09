0904f3f67c92ec0ca121e8b5f3ae08542201f7f4
Iman Ghanei <ghaneiash@gmail.com>
2022-02-09 10:37:03 +0100

    changes

--- src/components/Reducer/CountReducer.js
+++ src/components/Reducer/CountReducer.js
@@ -1,17 +1,18 @@
 import { useReducer } from "react";
 
-// Now let's do more complex state and action
-// Now let's to handle multiple state
-// Now with two property or two objects
+// Now if we give value in return -> <button onClick={() => dispatch({ type: "add", value: 1 })}> that means,
+// we can easily add another button for example -> <button onClick={() => dispatch({ type: "add", value: 5 })}>
 const initialState = {
   firstCounter: 0,
   secondCounter: 0,
 };
-
+// NOTICE So what did we do in this section? we made our action to object, so now our action has type and value
+// beside, our state is an object
 const reducer = (state, action) => {
   console.log(state, action);
   switch (action.type) {
     case "add":
+      // ...state -> firstly take a clone and after that override your state
       return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrement":
       return { ...state, firstCounter: state.firstCounter - action.value };
@@ -33,18 +34,28 @@ const CountReducer = () => {
     <div>
       <h2>count one is {count.firstCounter}</h2>
       <h2>count two is {count.secondCounter}</h2>
-      <button onClick={() => dispatch({ type: "add", value: 1 })}>
-        add one
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
-        decrement
-      </button>
-      <button onClick={() => dispatch({ type: "add2", value: 1 })}>
-        add one
-      </button>
-      <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
-        decrement
-      </button>
+      <div>
+        <button onClick={() => dispatch({ type: "add", value: 1 })}>
+          add one
+        </button>
+        <button onClick={() => dispatch({ type: "add", value: 5 })}>
+          add five
+        </button>
+        <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
+          decrement
+        </button>
+      </div>
+      <div>
+        <button onClick={() => dispatch({ type: "add2", value: 1 })}>
+          add one
+        </button>
+        <button onClick={() => dispatch({ type: "add2", value: 5 })}>
+          add five
+        </button>
+        <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
+          decrement
+        </button>
+      </div>
       <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
