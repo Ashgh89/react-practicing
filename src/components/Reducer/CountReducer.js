c1c032440a4600062509ad6db43f5a9bde090048
Iman Ghanei <ghaneiash@gmail.com>
2022-02-09 10:20:46 +0100

    changes

--- src/components/Reducer/CountReducer.js
+++ src/components/Reducer/CountReducer.js
@@ -2,7 +2,6 @@ import { useReducer } from "react";
 
 // Now let's do more complex state and action
 // Now let's to handle multiple state
-
 // Now with two property or two objects
 const initialState = {
   firstCounter: 0,
@@ -12,14 +11,14 @@ const initialState = {
 const reducer = (state, action) => {
   console.log(state, action);
   switch (action.type) {
-    case "addOne":
-      return { ...state, firstCounter: state.firstCounter + 1 };
+    case "add":
+      return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrement":
-      return { ...state, firstCounter: state.firstCounter - 1 };
-    case "addOne2":
-      return { ...state, secondCounter: state.secondCounter + 1 };
+      return { ...state, firstCounter: state.firstCounter - action.value };
+    case "add2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
     case "decrement2":
-      return { ...state, secondCounter: state.secondCounter - 1 };
+      return { ...state, secondCounter: state.secondCounter - action.value };
     case "reset":
       return initialState;
     default:
@@ -34,10 +33,16 @@ const CountReducer = () => {
     <div>
       <h2>count one is {count.firstCounter}</h2>
       <h2>count two is {count.secondCounter}</h2>
-      <button onClick={() => dispatch({ type: "addOne" })}>add one</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
-      <button onClick={() => dispatch({ type: "addOne2" })}>add one</button>
-      <button onClick={() => dispatch({ type: "decrement2" })}>
+      <button onClick={() => dispatch({ type: "add", value: 1 })}>
+        add one
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
+        decrement
+      </button>
+      <button onClick={() => dispatch({ type: "add2", value: 1 })}>
+        add one
+      </button>
+      <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
         decrement
       </button>
       <button onClick={() => dispatch({ type: "reset" })}>reset</button>
