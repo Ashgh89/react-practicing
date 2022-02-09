c530091b4c3030a28500c7d708eb69f63b0ffc24
Iman Ghanei <ghaneiash@gmail.com>
2022-02-09 11:19:24 +0100

    changes

--- src/components/Reducer/CountReducer.js
+++ src/components/Reducer/CountReducer.js
@@ -1,25 +1,13 @@
 import { useReducer } from "react";
 
-// Now if we give value in return -> <button onClick={() => dispatch({ type: "add", value: 1 })}> that means,
-// we can easily add another button for example -> <button onClick={() => dispatch({ type: "add", value: 5 })}>
-const initialState = {
-  firstCounter: 0,
-  secondCounter: 0,
-};
-// NOTICE So what did we do in this section? we made our action to object, so now our action has type and value
-// beside, our state is an object
+const initialState = 0;
 const reducer = (state, action) => {
   console.log(state, action);
   switch (action.type) {
     case "add":
-      // ...state -> firstly take a clone and after that override your state
-      return { ...state, firstCounter: state.firstCounter + action.value };
+      return state + action.value;
     case "decrement":
-      return { ...state, firstCounter: state.firstCounter - action.value };
-    case "add2":
-      return { ...state, secondCounter: state.secondCounter + action.value };
-    case "decrement2":
-      return { ...state, secondCounter: state.secondCounter - action.value };
+      return state - action.value;
     case "reset":
       return initialState;
     default:
@@ -27,36 +15,37 @@ const reducer = (state, action) => {
   }
 };
 
+// NOTICE Now let's use multiple useReducer, and with that we don't need change or switch (up there)
+// Because if we use the last method, our actions gonna by by many many codes
+// but here we hold our action stable and our state is just a basic number (const initialState = 0;) (not an object)
+// and we just add a second useReducer and that'S works well
+
 const CountReducer = () => {
   const [count, dispatch] = useReducer(reducer, initialState);
+  const [countTwo, dispatchTwo] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h2>count one is {count.firstCounter}</h2>
-      <h2>count two is {count.secondCounter}</h2>
+      <h2>count one is {count}</h2>
       <div>
-        <button onClick={() => dispatch({ type: "add", value: 1 })}>
-          add one
-        </button>
         <button onClick={() => dispatch({ type: "add", value: 5 })}>
           add five
         </button>
         <button onClick={() => dispatch({ type: "decrement", value: 1 })}>
           decrement
         </button>
+        <button onClick={() => dispatch({ type: "reset" })}>reset</button>
       </div>
+      <h2>count two is {countTwo}</h2>
       <div>
-        <button onClick={() => dispatch({ type: "add2", value: 1 })}>
-          add one
-        </button>
-        <button onClick={() => dispatch({ type: "add2", value: 5 })}>
+        <button onClick={() => dispatchTwo({ type: "add", value: 5 })}>
           add five
         </button>
-        <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
+        <button onClick={() => dispatchTwo({ type: "decrement", value: 1 })}>
           decrement
         </button>
+        <button onClick={() => dispatchTwo({ type: "reset" })}>reset</button>
       </div>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
 };
