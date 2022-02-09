aad82e62bf9a1389597209d713e949c7ccc93e80
Iman Ghanei <ghaneiash@gmail.com>
2022-02-09 10:08:35 +0100

    changes

--- src/components/Reducer/CountReducer.js
+++ src/components/Reducer/CountReducer.js
@@ -3,6 +3,7 @@ import { useReducer } from "react";
 // Now let's do more complex state and action
 // Now let's to handle multiple state
 
+// Now with two property or two objects
 const initialState = {
   firstCounter: 0,
   secondCounter: 0,
@@ -12,9 +13,13 @@ const reducer = (state, action) => {
   console.log(state, action);
   switch (action.type) {
     case "addOne":
-      return { firstCounter: state.firstCounter + 1 };
+      return { ...state, firstCounter: state.firstCounter + 1 };
     case "decrement":
-      return { firstCounter: state.firstCounter - 1 };
+      return { ...state, firstCounter: state.firstCounter - 1 };
+    case "addOne2":
+      return { ...state, secondCounter: state.secondCounter + 1 };
+    case "decrement2":
+      return { ...state, secondCounter: state.secondCounter - 1 };
     case "reset":
       return initialState;
     default:
@@ -27,9 +32,14 @@ const CountReducer = () => {
 
   return (
     <div>
-      <h2>count is {count.firstCounter}</h2>
+      <h2>count one is {count.firstCounter}</h2>
+      <h2>count two is {count.secondCounter}</h2>
       <button onClick={() => dispatch({ type: "addOne" })}>add one</button>
       <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
+      <button onClick={() => dispatch({ type: "addOne2" })}>add one</button>
+      <button onClick={() => dispatch({ type: "decrement2" })}>
+        decrement
+      </button>
       <button onClick={() => dispatch({ type: "reset" })}>reset</button>
     </div>
   );
